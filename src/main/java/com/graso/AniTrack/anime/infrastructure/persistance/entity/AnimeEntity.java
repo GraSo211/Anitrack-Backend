4206bce360943db550b19e376e8bcbc8484aab8b
Santiago <santigs211@gmail.com>
2025-12-02 00:11:47 -0300

    changes

---
+++ src/main/java/com/graso/AniTrack/anime/infrastructure/persistance/entity/AnimeEntity.java
@@ -0,0 +1,18 @@
+package com.graso.anitrack.anime.infrastructure.persistance.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="users")
+public final class AnimeEntity{
+    @Id
+    private final Long id;
+    private final Long malId;
+    private final String name;
+    
+}
